c4c0ba98e23008d709834f234cf61ceb49a8d292
Renan Costa <renan2727@hotmail.com>
2014-05-14 14:12:36 -0300

    Criação dos primeiros passos para o bd relacional

---
+++ acheiVinhos.pl
@@ -0,0 +1,42 @@
+id	Nome	Safra	Tipo	Uva	Teor Alcoólico	Amadurecimento	Temperatura	Nota
+vinho(cddrcs2011,'Casillero Del Diablo Reserva Cabernet Sauvignon',2011,'Tinto','Cabernet Sauvignon',13.5,carvalho8m,15,4).
+vinho(, , , , , , , ,).
+vinho(, , , , , , , ,).
+vinho(, , , , , , , ,).
+
+
+País	Região	Vinícola
+localizacao('Chile','Valle Central','Viña Concha Y Toro').
+localizacao(, ,).
+localizacao(, ,).
+
+
+Visual	Olfativo1	Olfativo2	Gustativo1	Gustativo2	Gustativo3	Vinho id
+caracteristicaVinho('Rubi intenso','Frutas negras','Especiarias','Macio','Equilibrado','Frutado',cddrcs2011).
+caracteristicaVinho(, , , , , , ,).
+caracteristicaVinho(, , , , , , ,).
+caracteristicaVinho(, , , , , , ,).
+
+
+id	barril	tempo
+amadurecimento(carvalho8m,carvalho,8).
+amadurecimento(, ,).
+amadurecimento(, ,).
+
+
+Nome	peso_sabores	momento	gordura	tipo
+harmonizacao('Carne assada', , , ,).
+harmonizacao('Canelone queijo', , , ,).
+harmonizacao('Costela no bafo', , , ,).
+harmonizacao('Linguiça na brasa', , , ,).
+harmonizacao('Pizza de calabresa', , , ,).
+harmonizacao(, , , ,).
+harmonizacao(, , , ,).
+
+
+País	Sigla	Adjetivo1	Adjetivo2	Clima
+caracteristicaPais(, , , ,).
+caracteristicaPais(, , , ,).
+caracteristicaPais(, , , ,).
+caracteristicaPais(, , , ,).
+caracteristicaPais(, , , ,).
---
+++ acheiVinhosTeste.pl
@@ -0,0 +1,20 @@
+vinho(cddrcs2011,'Casillero Del Diablo Reserva Cabernet Sauvignon',2011,'Tinto','Cabernet Sauvignon',13.5,carvalho8m,15,4).
+
+localizacao('Chile','Valle Central','Viña Concha Y Toro').
+
+caracteristicaVinho('Rubi intenso','Frutas negras','Especiarias','Macio','Equilibrado','Frutado',cddrcs2011).
+
+amadurecimento(carvalho8m,carvalho,8).
+
+prato('Carne assada', , , ,).
+prato('Canelone queijo', , , ,).
+prato('Costela no bafo', , , ,).
+prato('Linguiça na brasa', , , ,).
+prato('Pizza de calabresa', , , ,).
+
+origem(cddrcs2011,'Chile').
+harmonizacao(cddrcs2011,'Carne assada').
+harmonizacao(cddrcs2011,'Canelone queijo').
+harmonizacao(cddrcs2011,'Costela no bafo').
+harmonizacao(cddrcs2011,'Linguiça na brasa').
+harmonizacao(cddrcs2011,'Pizza de calabresa').
